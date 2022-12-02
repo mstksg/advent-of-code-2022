2281883e75e162fa09f46e02300a83e9f8c12ee9
Justin Lê <jusle@google.com>
2022-12-02 07:02:17 +0000

    abstract out the scoring in part 2

--- src/AOC/Challenge/Day02.hs
+++ src/AOC/Challenge/Day02.hs
@@ -30,28 +30,22 @@ matchLetter = \case
     'Z' -> Just 2
     _   -> Nothing
 
-scoreMatches
+day02
     :: (Z3 -> Z3 -> Z3)  -- ^ Get shape score
     -> (Z3 -> Z3 -> Z3)  -- ^ Get outcome score
-    -> [(Z3, Z3)]
-    -> Integer
-scoreMatches f g = sum . map go
+    -> [(Z3, Z3)] :~> Integer
+day02 f g = MkSol
+    { sParse = traverse parseLine . lines
+    , sShow  = show
+    , sSolve = Just . sum . map go
+    }
   where
-   go (x, y) = getFinite (f x y) + getFinite (g x y) * 3 + 1
+    parseLine = listTup . mapMaybe matchLetter
+    go (x, y) = getFinite (f x y) + getFinite (g x y) * 3 + 1
 
-parseLine :: String -> Maybe (Z3, Z3)
-parseLine = listTup . mapMaybe matchLetter
 
 day02a :: [(Z3, Z3)] :~> Integer
-day02a = MkSol
-    { sParse = traverse parseLine . lines
-    , sShow  = show
-    , sSolve = Just . scoreMatches (\_ y -> y) (\x y -> y - x + 1)
-    }
+day02a = day02 (\_ y -> y) (\x y -> y - x + 1)
 
 day02b :: [(Z3, Z3)] :~> Integer
-day02b = MkSol
-    { sParse = traverse parseLine . lines
-    , sShow  = show
-    , sSolve = Just . scoreMatches (\x y -> x + y + 2) (\_ y -> y)
-    }
+day02b = day02 (\x y -> x + y + 2) (\_ y -> y)
