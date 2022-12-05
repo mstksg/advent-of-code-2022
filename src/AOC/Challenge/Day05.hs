062f552ff6cfd5dbaf3e239847bcc0400f7b3d2e
Justin Lê <jusle@google.com>
2022-12-05 19:34:21 +0000

    initial day 5

--- src/AOC/Challenge/Day05.hs
+++ src/AOC/Challenge/Day05.hs
@@ -22,8 +22,8 @@
 --     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day05 (
-    -- day05a
-  -- , day05b
+    day05a
+  , day05b
   ) where
 
 import           AOC.Prelude
@@ -41,20 +41,70 @@ import qualified Data.Set                       as S
 import qualified Data.Text                      as T
 import qualified Data.Vector                    as V
 import qualified Linear                         as L
+import           Data.Bitraversable (bitraverse)
 import qualified Text.Megaparsec                as P
 import qualified Text.Megaparsec.Char           as P
 import qualified Text.Megaparsec.Char.Lexer     as PP
 
+
+data Instr = Instr { amount :: !Int, source :: !Int, dest :: !Int }
+  deriving stock (Eq, Show, Generic)
+
+instance NFData Instr
+
+parseInput
+    :: String
+    -> Maybe (IntMap [Char], [Instr])
+parseInput = bitraverse (parseGrid . lines) (parseInstr . lines)
+         <=< listTup . splitOn "\n\n"
+  where
+    parseGrid  = Just . IM.fromList . zip [1..]
+               . filter (not . null) . map (filter isLetter)
+               . transpose
+    parseInstr = traverse $
+          fmap (\(V3 amount source dest) -> Instr{..})
+        . listV3
+      <=< traverse readMaybe
+        . words
+        . clearOut (not . isDigit)
+
+runInstr
+    :: (Int -> [a] -> [a] -> ([a], [a]))
+    -> IntMap [a]
+    -> Instr
+    -> IntMap [a]
+runInstr f stacks (Instr{..}) = IM.fromList [(source, xs'), (dest, ys')] `IM.union` stacks
+  where
+    xs = stacks IM.! source
+    ys = stacks IM.! dest
+    (xs', ys') = f amount xs ys
+
 day05a :: _ :~> _
 day05a = MkSol
-    { sParse = Just . lines
-    , sShow  = show
-    , sSolve = Just
+    { sParse = parseInput
+    , sShow  = id
+    , sSolve = \(stacks, instrs) ->
+          traverse listToMaybe
+        . toList
+        $ foldl' (runInstr shiftItems) stacks instrs
     }
+  where
+    shiftItems :: Int -> [a] -> [a] -> ([a],[a])
+    shiftItems 0 xs    ys  = (xs, ys)
+    shiftItems _ []    ys  = ([], ys)
+    shiftItems n (x:xs) ys = shiftItems (n-1) xs (x:ys)
 
 day05b :: _ :~> _
 day05b = MkSol
     { sParse = sParse day05a
-    , sShow  = show
-    , sSolve = Just
+    , sShow  = id
+    , sSolve = \(stacks, instrs) ->
+          traverse listToMaybe
+        . toList
+        $ foldl' (runInstr shiftItems) stacks instrs
     }
+  where
+    shiftItems :: Int -> [a] -> [a] -> ([a],[a])
+    shiftItems n xs ys  = (xs', zs ++ ys)
+      where
+        (zs,xs') = splitAt n xs
