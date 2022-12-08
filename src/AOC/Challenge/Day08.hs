81369cf67631aab80f825e05113aa7b15d3df9be
Justin Lê <jusle@google.com>
2022-12-08 20:54:56 +0000

    day 8 approach changed

--- src/AOC/Challenge/Day08.hs
+++ src/AOC/Challenge/Day08.hs
@@ -12,59 +12,47 @@ module AOC.Challenge.Day08 (
   , day08b
   ) where
 
-import           AOC.Common (countTrue)
-import           AOC.Common.Point (Point)
+import           AOC.Common (countTrue, digitToIntSafe)
 import           AOC.Solver ((:~>)(..))
-import           Data.Char (digitToInt)
-import           Data.IntMap (IntMap)
-import           Data.List (findIndex, transpose)
-import           Linear (V2(..), V4(..))
+import           Data.List (transpose, mapAccumL, foldl')
 import           Safe.Foldable (maximumMay)
-import qualified Data.IntMap as IM
+import qualified Data.Map as M
 
-crossSections
-    :: String
-    -> (IntMap [Int], IntMap [Int])
-crossSections xs = ( IM.fromList (zip [0..] horizLines)
-                   , IM.fromList (zip [0..] vertLines)
-                   )
+onSightLines
+    :: ([Int] -> [a])
+    -> (a -> a -> a)
+    -> [[Int]]
+    -> [a]
+onSightLines f g rows = concat $
+    foldl'
+      ((zipWith . zipWith) g)
+      leftLines
+      [rightLines, upLines, downLines]
   where
-    horizLines = map (map digitToInt) $ lines xs
-    vertLines  = transpose horizLines
+    leftLines  = map f rows
+    rightLines = map (reverse . f . reverse) rows
+    upLines    = transpose $ map f (transpose rows)
+    downLines  = transpose $ map (reverse . f . reverse) (transpose rows)
 
-visibility
-    :: (IntMap [Int], IntMap [Int])
-    -> Point
-    -> V4 (Bool, Int)
-visibility (horizLines, vertLines) (V2 x y) =
-    reshape <$> V4 toUp toRight toDown toLeft
-  where
-    (reverse->toLeft, hereAndToRight) = splitAt x (horizLines IM.! y)
-    (reverse->toUp  , tail->toDown  ) = splitAt y (vertLines  IM.! x)
-    here    = head hereAndToRight
-    toRight = tail hereAndToRight
-    reshape ls = case findIndex (>= here) ls of
-      Nothing -> (True , length ls)
-      Just i  -> (False, i + 1    )
-
-day08a :: _ :~> _
+day08a :: [[Int]] :~> Int
 day08a = MkSol
-    { sParse = Just . crossSections
+    { sParse = (traverse . traverse) digitToIntSafe . lines
     , sShow  = show
-    , sSolve = \cs@(horiz, vert) -> Just . countTrue or $ [
-          fst <$> visibility cs (V2 x y)
-        | y <- IM.keys horiz
-        , x <- IM.keys vert
-        ]
+    , sSolve = Just . countTrue id
+             . onSightLines (snd . mapAccumL propagateSight (-1)) (||)
     }
+  where
+    propagateSight i x = (max i x, x > i)
 
-day08b :: _ :~> _
+day08b :: [[Int]] :~> Int
 day08b = MkSol
-    { sParse = Just . crossSections
+    { sParse = (traverse . traverse) digitToIntSafe . lines
     , sShow  = show
-    , sSolve = \cs@(horiz, vert) -> maximumMay [
-          product $ snd <$> visibility cs (V2 x y)
-        | y <- IM.keys horiz
-        , x <- IM.keys vert
-        ]
+    , sSolve = maximumMay
+             . onSightLines (snd . mapAccumL findSight M.empty . zip [0..]) (*)
     }
+  where
+    findSight lastSeeable (i, x) =
+      ( M.fromList ((,i) <$> [0..x]) `M.union` lastSeeable
+      , i - M.findWithDefault 0 x lastSeeable
+      )
